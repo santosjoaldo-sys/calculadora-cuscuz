39b0e6cb71bbdff221854920bfc41cbff23df78d
santosjoaldo-sys <santos.joaldo@gmail.com>
2026-03-16 20:05:04 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,40 @@
+const CACHE_NAME = "comida-lucrativa-v1";
+
+const urlsToCache = [
+"/",
+"/index.html",
+"/gerador-cardapio.html",
+"/gerador-nomes.html",
+"/gerador-combos.html",
+"/gerador-post.html",
+"/icon-192.png",
+"/icon-512.png"
+];
+
 self.addEventListener("install", function(event) {
-console.log("Service Worker instalado");
+
+event.waitUntil(
+
+caches.open(CACHE_NAME).then(function(cache) {
+
+return cache.addAll(urlsToCache);
+
+})
+
+);
+
 });
 
-self.addEventListener("fetch", function(event) {});
+self.addEventListener("fetch", function(event) {
+
+event.respondWith(
+
+caches.match(event.request).then(function(response) {
+
+return response || fetch(event.request);
+
+})
+
+);
+
+});
